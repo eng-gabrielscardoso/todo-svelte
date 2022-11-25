cc6552d95395cfd901424fb4b27438ae87e08d7e
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-11-24 23:36:56 -0300

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import Card from './components/base/Card.svelte'
+</script>
+
+<main class="h-screen grid place-items-center bg-gray-900">
+  <Card title="TODO App"></Card>
+</main>
---
+++ src/components/base/Card.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  export let title: string = 'Card Title'
+</script>
+
+<div class="bg-white rounded-xl p-4">
+  <h2 class="text-2xl text-center font-bold text-gray-900">{ title }</h2>
+  <slot />
+</div>
